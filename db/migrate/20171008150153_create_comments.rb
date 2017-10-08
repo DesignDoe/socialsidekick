7d44882ac5fecee5c49ea7c1072ec29cc651c231
DesignDoe <support@designdoe.com>
2017-10-08 15:06:59 +0000

    set up prouction for mailers

--- db/migrate/20171008150153_create_comments.rb
+++ db/migrate/20171008150153_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.refernces :user
+      t.references :user, index: true, foreign_key: true
       t.references :article, index: true, foreign_key: true
 
       t.timestamps null: false
